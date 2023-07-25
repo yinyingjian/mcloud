921949050d14f91ed5ffbb342d171b70acb7cfe2
yumaojun03 <18108053819@163.com>
2023-07-25 09:49:16 +0800

    补充登录

--- src/App.vue
+++ src/App.vue
@@ -2,11 +2,33 @@
 </script>
 
 <template>
-  <header>
-  </header>
-
-  <RouterView />
+  <!-- 顶部导航 -->
+  <div>
+    <a-space>
+      <!-- 导航区 -->
+      <div class="nav">
+        <!-- logo显示区 -->
+        <div>Go11微博客系统</div>
+        <!-- 系统菜单 -->
+        <div>系统菜单</div>
+        <!-- 登录操作区 -->
+        <div class="to-backend">
+          <a-space>
+            <!-- 模板里面使用ref对象，不需要添加value -->
+            <a-button @click="JumpToBackend">后台管理</a-button>
+            <a-button @click="JumpToFrontend">前台浏览</a-button>
+            <a-button @click="Logout">退出</a-button>
+            <a-button @click="Login">登录</a-button>
+          </a-space>
+        </div>
+      </div>
+    </a-space>
+  </div>
+  <!-- Layout布局页面 -->
+  <!-- 这是我们的根页面,所有的页面都需要嵌入到根页面才能成形出来, 特别是Vue Route的子页面 -->
+  <div>
+    <RouterView />
+  </div>
 </template>
 
-<style scoped>
-</style>
+<style scoped></style>
---
+++ src/api/mcenter/token.js
@@ -0,0 +1,6 @@
+import instance from '../client'
+
+// 登录
+export var ISSUE_TOKEN = (data) => {
+    return instance.post('/mcenter/api/v1/token', data)
+}
