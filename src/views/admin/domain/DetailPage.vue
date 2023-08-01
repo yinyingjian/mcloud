bff8aabb78b9593dfc63b89a6ddf40f00e60666d
yumaojun03 <18108053819@163.com>
2023-08-01 16:55:34 +0800

    替换URL

--- src/views/admin/domain/DetailPage.vue
+++ src/views/admin/domain/DetailPage.vue
@@ -5,6 +5,17 @@ import LdpaSetting from './components/LdpaSetting.vue'
 import FeishuSetting from './components/FeishuSetting.vue'
 import { GET_DOMAIN } from '@/api/mcenter/domain'
 import { onBeforeMount, ref } from 'vue'
+import { useRouter } from 'vue-router'
+
+const router = useRouter()
+const tab = ref(router.currentRoute.value.query.tab)
+if (!tab.value) {
+  tab.value = 'PASSWORD'
+}
+const ChangeTab = (v) => {
+  tab.value = v
+  router.replace({ query: { tab: v } })
+}
 
 const settings = ref({
   security_setting: {}
@@ -22,15 +33,15 @@ onBeforeMount(async () => {
 
 <template>
   <div>
-    <a-tabs :size="app.size">
+    <a-tabs :size="app.size" :active-key="tab" @change="ChangeTab">
       <a-tab-pane key="PASSWORD" title="本地用户">
         <LocalUser :security-setting="settings.security_setting" />
       </a-tab-pane>
       <a-tab-pane key="LDAP" title="LDAP">
-        <LdpaSetting :ldap-setting="settings.ldap_setting" />
+        <LdpaSetting :ldap-setting="settings.ldap_setting" v-if="settings.ldap_setting" />
       </a-tab-pane>
       <a-tab-pane key="FEISHU" title="飞书登录">
-        <FeishuSetting :feishu-setting="settings.feishu_setting" />
+        <FeishuSetting :feishu-setting="settings.feishu_setting" v-if="settings.feishu_setting" />
       </a-tab-pane>
     </a-tabs>
   </div>
--- src/views/admin/domain/components/FeishuSetting.vue
+++ src/views/admin/domain/components/FeishuSetting.vue
@@ -1,5 +1,5 @@
 <script setup>
-import { reactive } from 'vue'
+import { onMounted, reactive, ref } from 'vue'
 
 // 声明属性
 const props = defineProps({
@@ -8,22 +8,48 @@ const props = defineProps({
 
 // 表单
 const form = reactive({
-  enabled: props.enabled
+  enabled: false,
+  app_id: '',
+  app_secret: '',
+  redirect_uri: ''
+})
+const initForm = () => {
+  form.enabled = props.feishuSetting.enabled
+  form.app_id = props.feishuSetting.app_id
+  form.app_secret = ''
+  form.redirect_uri = props.feishuSetting.redirect_uri
+}
+onMounted(() => {
+  initForm()
 })
 
-//
+// 提交表单
+const submitLoading = ref(false)
 const handleSubmit = () => {}
 </script>
 
 <template>
   <div class="page">
     <a-card>
-      <a-form :model="form" @submit="handleSubmit">
+      <a-form :model="form" @submit="handleSubmit" auto-label-width>
         <a-form-item field="enabled" label="启动">
           <a-switch type="round" v-model="form.enabled" />
         </a-form-item>
+        <a-form-item field="app_id" label="App ID" :required="form.enabled">
+          <a-input v-model="form.app_id"> </a-input>
+        </a-form-item>
+        <a-form-item field="app_secret" label="App Secret">
+          <a-input v-model="form.app_secret"> </a-input>
+        </a-form-item>
+        <a-form-item field="redirect_uri" label="重定向地址" :required="form.enabled">
+          <a-input v-model="form.redirect_uri"> </a-input>
+        </a-form-item>
       </a-form>
-      {{ feishuSetting }}
+      <div class="form-submit">
+        <a-space>
+          <a-button type="primary" html-type="submit" :loading="submitLoading">保存</a-button>
+        </a-space>
+      </div>
     </a-card>
   </div>
 </template>
